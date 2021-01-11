75377d8c6d5100de9b631bf29614445b961c2c4a
GodU <827586410@qq.com>
2021-01-11 21:37:09 +0800

    impl WordCount.java

--- src/main/java/com/sty/wc/WordCount.java
+++ src/main/java/com/sty/wc/WordCount.java
@@ -1,5 +1,30 @@
 package com.sty.wc;
 
-public class WordCount {
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
 
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        String inputPath = "/Users/sty/IdeaProjects/flink-test/src/main/resources/word.txt";
+        DataSet<String> source = env.readTextFile(inputPath);
+        source.flatMap(new MyflatMapper())
+                .groupBy(0)
+                .sum(1)
+                .print();
+    }
+
+    public static class MyflatMapper implements FlatMapFunction<String, Tuple2<String,Integer>>{
+
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            String[] words = s.split(" ");
+            for (String word : words) {
+                collector.collect(new Tuple2<>(word,1));
+            }
+        }
+    }
 }
